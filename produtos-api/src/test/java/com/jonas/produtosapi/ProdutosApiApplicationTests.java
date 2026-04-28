be4a1e037796072ccdebefc61d45d206014a3a80
JonasMoura-J <jonasmourat9090@gmail.com>
2026-04-27 23:29:26 -0300

    adicionando primeiro projeto

---
+++ produtos-api/src/test/java/com/jonas/produtosapi/ProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonas.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
