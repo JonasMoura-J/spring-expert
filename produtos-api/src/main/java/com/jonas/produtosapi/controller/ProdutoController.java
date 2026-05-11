8005abda33b983e2dafdc12b69a7b287dbfe6732
jonasmt7 <jonasmourat9090@gmail.com>
2026-05-11 15:18:19 -0300

    Adicionando classe de produto

---
+++ produtos-api/src/main/java/com/jonas/produtosapi/controller/ProdutoController.java
@@ -0,0 +1,14 @@
+package com.jonas.produtosapi.controller;
+
+import com.jonas.produtosapi.model.Produto;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("produtos")
+public class ProdutoController {
+
+    public void salvar(Produto produto){
+
+    }
+}
